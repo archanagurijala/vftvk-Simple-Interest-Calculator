9de31eead1bb73d5159082465fac4de3700d2981
Archana Gurijala <archana.gurijala16@gmail.com>
2021-08-08 16:58:13 +0530

    Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function displayROI() {
+    var rate = document.getElementById("rate").value;
+    document.getElementById("roi").innerHTML = rate;
+};
+
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if(principal <=0 ) {
+        alert("enter a positive number");
+        document.getElementById('principal').focus();
+    }
     
-}
-        
+    var simpleInterest = parseInt(principal * rate * years)/100;
+    var year =  new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("deposit").innerHTML = principal;
+    document.getElementById("interest").innerHTML = rate;
+    document.getElementById("amount").innerHTML = simpleInterest;
+    document.getElementById("year").innerHTML = year;
+}        
